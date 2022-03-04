3d9881ebfb94faab504fe0d9b6a17b0890ef32f4
louiscaoUT <100396043+louiscaoUT@users.noreply.github.com>
2022-03-03 19:00:05 -0600

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,12 @@
+
+function triangleArea(base, height) {
+    return 1/2 * (base * height);
+}
+
+var area = triangleArea(5, 10);
+var areaAgain = triangleArea(2, 4);
+var areaFinal = triangleArea(4, 20);
+
+console.log(area);
+console.log(areaAgain);
+console.log(areaFinal);
