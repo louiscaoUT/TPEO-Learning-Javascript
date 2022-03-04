11b479ae6e0441ea9de6c868bea20fff427750fa
louiscaoUT <100396043+louiscaoUT@users.noreply.github.com>
2022-03-03 19:28:39 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -11,13 +11,13 @@ function convertFeetToInches(length){
     return (length * 12);
 }
 
-var length_1 = convertFeetToInches(1)
-var length_3 = convertFeetToInches(3)
-var length_30 = convertFeetToInches(30)
+var length_1 = convertFeetToInches(1);
+var length_3 = convertFeetToInches(3);
+var length_30 = convertFeetToInches(30);
 
 console.log(area);
 console.log(areaAgain);
 console.log(areaFinal);
-console.log(length_1)
-console.log(length_3)
-console.log(length_30)
+console.log(length_1);
+console.log(length_3);
+console.log(length_30);
