a31bb9516a6e697e402225c3f060868a62ff90c7
louiscaoUT <100396043+louiscaoUT@users.noreply.github.com>
2022-03-03 19:27:11 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -7,6 +7,17 @@ var area = triangleArea(5, 10);
 var areaAgain = triangleArea(2, 4);
 var areaFinal = triangleArea(4, 20);
 
+function convertFeetToInches(length){
+    return (length * 12);
+}
+
+var length_1 = convertFeetToInches(1)
+var length_3 = convertFeetToInches(3)
+var length_30 = convertFeetToInches(30)
+
 console.log(area);
 console.log(areaAgain);
 console.log(areaFinal);
+console.log(length_1)
+console.log(length_3)
+console.log(length_30)
