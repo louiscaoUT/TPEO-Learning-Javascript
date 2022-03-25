85efc347e7ebde94b2f7127e3545648d8a7e298e
louiscaoUT <100396043+louiscaoUT@users.noreply.github.com>
2022-03-24 21:46:12 -0500

    Update Homework_3.js

--- Homework_3.js
+++ Homework_3.js
@@ -1,23 +1,23 @@
 // Question 1:
 
-// function powersOfThree() { // A function that prints out powers of 3 up until the 9th power
-//     count = 9
-//     startCount = 3
+function powersOfThree() { // A function that prints out powers of 3 up until the 9th power
+    count = 9
+    startCount = 3
     
-//     for (var i = 0; i <= count; i++) {
-//         result = startCount ** i;
-//         console.log(result);
+    for (var i = 0; i <= count; i++) {
+        result = startCount ** i;
+        console.log(result);
         
-//         if (i == 1) {
-//            console.log("This is my favorite power of three!")
+        if (i == 1) {
+           console.log("This is my favorite power of three!")
             
-//         }
-//     }
+        }
+    }
     
 
-// }
+}
 
-// var timeIDpowersOfThree = setTimeout(powersOfThree, 3000)
+var timeIDpowersOfThree = setTimeout(powersOfThree, 3000)
 
 // Question 2
 
